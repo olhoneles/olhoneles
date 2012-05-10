a2d44565823b34fb18a4c74bf838eb295e9932ad
Gustavo Noronha Silva <gns@gnome.org>
2012-05-09 22:31:54 -0300

    Make totals per legislator a table

--- base/models/totals.py
+++ base/models/totals.py
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy import Table, Column, Unicode, Float
+from sqlalchemy import Table, Column, Unicode, Integer, Float
 
 from base.models import Base
 
@@ -29,4 +29,16 @@ class TotalsPerNature(Base):
 
     def __unicode__(self):
         return u'%s - %.2f' % (self.nature, self.expensed)
+
+
+class TotalsPerLegislator(Base):
+    __tablename__ = 'totals_per_legislator'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(Unicode)
+    party = Column(Unicode)
+    expensed = Column(Float)
+
+    def __unicode__(self):
+        return u'%s (%s) expensed %.2f' % (self.name, self.party, self.expensed)
 
