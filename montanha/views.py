33464c8d0e6a537c9da0b26f59879d11f5910798
Marcelo Jorge Vieira <metal@alucinados.com>
2013-06-16 20:07:11 -0300

    Intial version of new Layout

--- montanha/views.py
+++ montanha/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+#
+# Copyright (©) 2013 Marcelo Jorge Vieira <metal@alucinados.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.shortcuts import render
+
+
+def show_index(request):
+
+    c = {}
+
+    return render(request, 'index.html', c)
