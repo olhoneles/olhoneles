a50e974a771a647e51ca32d8125e9d16dde8a357
Gustavo Noronha Silva <gns@gnome.org>
2013-07-06 14:47:46 -0300

    Proper formatting of currency

--- montanha/views.py
+++ montanha/views.py
@@ -16,9 +16,13 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import locale
 from django.shortcuts import render
 from django.db.models import Sum, Count
 from montanha.models import *
+
+
+locale.setlocale(locale.LC_MONETARY, "")
 
 
 def show_index(request):
