f7d8ac1c6ffe1c23d8fa35bbe96e6745b86dbf40
Gustavo Noronha Silva <gns@gnome.org>
2013-07-06 12:25:57 -0300

    Add per-party total view

--- montanha/views.py
+++ montanha/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (©) 2013 Marcelo Jorge Vieira <metal@alucinados.com>
+# Copyright (©) 2013 Gustavo Noronha Silva <gustavo@noronha.eti.br>
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as
@@ -16,7 +17,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.shortcuts import render
-from django.db.models import Sum
+from django.db.models import Sum, Count
 from montanha.models import *
 
 
@@ -45,3 +46,19 @@ def show_per_legislator(request):
     c = {'data': data}
 
     return render(request, 'per_legislator.html', c)
+
+
+def show_per_party(request):
+    data = PoliticalParty.objects.raw("select montanha_politicalparty.id, "
+                                      "siglum, count(distinct(montanha_legislator.id)) as n_legislators, "
+                                      "sum(montanha_expense.expensed) as expensed_sum, "
+                                      "sum(montanha_expense.expensed) / count(distinct(montanha_legislator.id)) as expensed_average "
+                                      "from montanha_politicalparty, montanha_mandate, montanha_legislator, montanha_expense "
+                                      "where montanha_politicalparty.id = montanha_mandate.party_id and "
+                                      "montanha_mandate.legislator_id = montanha_legislator.id and "
+                                      "montanha_expense.mandate_id = montanha_mandate.id "
+                                      "group by siglum order by expensed_average desc")
+
+    c = {'data': list(data)}
+
+    return render(request, 'per_party.html', c)
