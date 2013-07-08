ea4da5d9f5e14f5764e895bc9e2029a1e917ac6d
Gustavo Noronha Silva <gns@gnome.org>
2013-07-08 08:44:20 -0300

    Improve graphing in the per-nature view
    Change the graph to a time series, make it an area graph showing absolute
    values, try to align it with the table showing the data.

--- montanha/views.py
+++ montanha/views.py
@@ -17,6 +17,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import locale
+from datetime import date
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.db.models import Sum, Count
@@ -38,7 +39,25 @@ def show_per_nature(request):
     data = Expense.objects.values('nature__name')
     data = data.annotate(expensed=Sum('expensed')).order_by('-expensed')
 
-    c = {'data': data}
+    years = [d.year for d in Expense.objects.dates('date', 'year')]
+
+    # We use the data variable to get our list of expense natures so that we can
+    # match the graph stacking with the order of the table rows
+    time_series = []
+    for nature_name in [d["nature__name"] for d in reversed(data)]:
+        nature = ExpenseNature.objects.get(name=nature_name)
+
+        l = []
+        time_series.append(l)
+
+        for year in years:
+            year_data = Expense.objects.filter(nature=nature)
+            year_data = year_data.filter(date__year=year)
+            year_data = year_data.values("nature__name")
+            year_data = year_data.annotate(expensed=Sum("expensed"))
+            l.append([int(date(year, 1, 1).strftime("%s000")), float(year_data[0]["expensed"])])
+
+    c = {'data': data, 'years_data': time_series}
 
     return render(request, 'per_nature.html', c)
 
