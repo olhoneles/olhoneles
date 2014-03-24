499c2a0a484d7d0218827d0cd68ab1134f9a9d06
Marcelo Jorge Vieira <metal@alucinados.com>
2014-03-23 22:29:40 -0300

    Views: Refactoring imports
    Removing unnecessary imports

--- montanha/views.py
+++ montanha/views.py
@@ -17,20 +17,20 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import locale
-import pickle
 import json
 from colorsys import hsv_to_rgb
 from datetime import date
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render as original_render
-from django.db.models import Sum, Count, Q
+from django.db.models import Sum, Q
 from django.http import HttpResponse
-from django.core.cache import cache
 from django.core.mail import send_mail
 from django.conf import settings
 from montanha.models import *
 from montanha.forms import *
-from montanha.util import filter_for_institution, get_date_ranges_from_data, ensure_years_in_range
+from montanha.util import (
+    filter_for_institution, get_date_ranges_from_data, ensure_years_in_range
+)
 
 locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
 locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
