c2ad302914f61aaec500d07ee337e1f97f384925
Marcelo Jorge Vieira <metal@alucinados.com>
2014-04-02 22:35:38 -0300

    Adding classifier Audience

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ legislators
     license='AGPL',
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Classifier: Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
