fa951d3944891bf3cc28b59ce65406dffb31bf33
Marcelo Jorge Vieira <metal@alucinados.com>
2016-12-17 19:55:12 -0200

    Collect: Remove unused imports

--- montanha/management/commands/collect.py
+++ montanha/management/commands/collect.py
@@ -15,9 +15,6 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import signal
-import sys
-
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
