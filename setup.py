58d206cfb6d79fb9280986c056a521058083e954
Marcelo Jorge Vieira <metal@alucinados.com>
2018-11-25 14:50:24 -0200

    Bump request to 2.20.x

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ legislators
         'python-dateutil>=2.6.0,<2.7.0',
         'python-slugify>=1.2.4,<1.3.0',
         'raven>=6.2.1,<6.3.0',
-        'requests>=2.6.0,<2.7.0',
+        'requests>=2.20.0,<2.21.0',
         'rows>=0.3.1,<0.4.0',
         'cachetools>=2.0.1,<2.1.0',
     ],
